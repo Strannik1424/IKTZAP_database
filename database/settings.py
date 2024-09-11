f9ccbe0f847a747ca3efd10bb5625d2206c9a3e4
Stranniy1424 <stranniy1424@gmail.com>
2024-09-11 17:26:28 +0900

    add postgresql

--- database/settings.py
+++ database/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1ee(u**kqwrmfqs#)(*2a07n1l2%@2#9vtg-@l^=owedo48jun'
+# SECRET_KEY = 'django-insecure-1ee(u**kqwrmfqs#)(*2a07n1l2%@2#9vtg-@l^=owedo48jun'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -75,30 +78,33 @@ WSGI_APPLICATION = 'database.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-import dj_database_url
+
 
 # DATABASES = {
 #     'default': dj_database_url.config(default='postgres://postgres:123@localhost:5432/PostgreSQL16')
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'iktzap',  # Замените на фактическое имя базы данных
+#         'USER': 'iktzap_admin',  # Имя пользователя базы данных
+#         'PASSWORD': 'admin',  # Пароль пользователя базы данных
+#         'HOST': 'localhost',  # Адрес хоста базы данных
+#         'PORT': '5432',  # Порт подключения к базе данных
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'iktzap',  # Замените на фактическое имя базы данных
-        'USER': 'iktzap_admin',  # Имя пользователя базы данных
-        'PASSWORD': 'admin',  # Пароль пользователя базы данных
-        'HOST': 'localhost',  # Адрес хоста базы данных
-        'PORT': '5432',  # Порт подключения к базе данных
-            
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Путь к файлу базы данных SQLite
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',  # Путь к файлу базы данных SQLite
-#     }
-# }
+database_url = os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
+
 
 
 # Password validation
